bc8a699e00168210f203811610bbd579f0e979da
Subash-G-S <subashgopi2006@gmail.com>
2026-05-18 18:07:41 +0530

    username box while signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,21 +1,51 @@
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import {
+  doc,
+  setDoc,
+} from "firebase/firestore";
 
+import { db } from "../firebase";
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
+  const [username, setUsername] =
+    useState("");
   const handleSignup = async (e) => {
     e.preventDefault();
 
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
 
-      alert("Account Created");
-    } catch (error) {
-      alert(error.message);
+  const userCredential =
+    await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+  const user = userCredential.user;
+
+  await setDoc(
+    doc(db, "users", user.uid),
+    {
+      name,
+      username,
+      email,
+
+      createdAt: new Date(),
     }
+  );
+
+  alert("Account Created");
+
+} catch (error) {
+
+  alert(error.message);
+
+}
   };
 
   return (
@@ -23,6 +53,21 @@ function Signup() {
       <h1>Signup</h1>
 
       <form onSubmit={handleSignup}>
+        <input
+  type="text"
+  placeholder="Full Name"
+  onChange={(e) =>
+    setName(e.target.value)
+  }
+/>
+
+<input
+  type="text"
+  placeholder="Username"
+  onChange={(e) =>
+    setUsername(e.target.value)
+  }
+/>
         <input
           type="email"
           placeholder="Email"
